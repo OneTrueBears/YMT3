8e197ee767a16dedce8eed0e4786c228ba4a48a3
Bjørnar Øvstedal <bovstedal95@gmail.com>
2020-11-23 16:43:14 +0100

    Bug Fix
    Fixed the "NaN" number bug.
    Cleaned up some unused code.

--- zoom.js
+++ zoom.js
@@ -20,16 +20,12 @@ gridCtx.canvas.x = 0;
 gridCtx.canvas.y = 0;
 
 var map = document.getElementById("mapWrapper");
-var mapImage = document.getElementById("map");
 
-console.log("(Map) " + "Height: " + mapCtx.canvas.height + " - " + "Width: " + mapCtx.canvas.width, "x: " + mapCtx.canvas.x + " - y: " + mapCtx.canvas.y);
+var mapImage = document.getElementById("map");
 
 sliderCtx.font = "15px Arial";
 sliderCtx.fillStyle = "white";
 mapCtx.font = "15px Arial";
-//mapCtx.fillStyle = "white";
-gridCtx.strokeStyle = "white";
-gridCtx.fillStyle = "white";
 
 var maxAltitude = 10000;
 var minAltitude = 0;
@@ -45,12 +41,15 @@ var mappedAltitude = (totalAltitude)/(window.innerHeight-(partition*2));
 //scrubby zoom. scale determines how far off the zooming is from starting value (1). This value is crucial for most other features.
 var scale = 2;
 var panning = false;
-var xoff = 400;
-var yoff = partition;
+var xoff = (window.innerWidth/2)*-1;
+var yoff = (window.innerHeight/2)*-1;
 var start = {x: 0, y: 0};
 setTransform();
 
-var mapHeight = mapImage.height * scale;
+console.log("(Map) " + "Height: " + mapCtx.canvas.height + " - " + "Width: " + mapCtx.canvas.width, "x: " + mapCtx.canvas.x + " - y: " + mapCtx.canvas.y);
+console.log("(Map) " + "Height: " + mapImage.height + " - " + "Width: " + mapImage.width, "x: " + mapImage.x + " - y: " + mapImage.y);
+
+var mapHeight = map.height * scale;
 var articles = getArticles();
 
 calculateAltitudes();
@@ -58,20 +57,17 @@ calculateAltitudes();
 //Calculate and write the altitudes associated with the mountain. 
 function calculateAltitudes(){
 	mapHeight = mapImage.height * scale;
+	var mapRatio = (totalAltitude)/(mapHeight);
 	var screenOccupationPercentage = (window.innerHeight-partition*2)/mapHeight;
 	sliderCtx.clearRect(0, 0, canvas1.width, canvas1.height);
-	var mapRatio = (totalAltitude)/(mapHeight);
 	for(position = 1; position<=quantity; position++){
-		var float = (maxAltitude + yoff*mapRatio - partition*mapRatio) - (position-1)*(((mapHeight*screenOccupationPercentage)*mapRatio)/(quantity-1));
+		var float = (totalAltitude + yoff*mapRatio - partition*mapRatio) - (position-1)*(((mapHeight*screenOccupationPercentage)*mapRatio)/(quantity-1));
 		var number = Math.trunc(float);
 		sliderCtx.fillStyle = "rgb(255, 255, 255)";
 		if(position > quantity*(4/5)){
 			sliderCtx.fillStyle = "rgb(0, 0, 0)";
 		}
 		sliderCtx.fillText(number.toString(), canvas1.width/3, partition*position);
-		/*gridCtx.beginPath();
-		gridCtx.rect(0, partition*position, window.innerWidth, 1);
-		gridCtx.stroke();*/
 	}
 	scaleArticles();
 }
@@ -87,24 +83,13 @@ function mapAltitudeToScreen(number){
 }
 
 function scaleArticles(){
-	/*for(var article = 0; article < articles.length; article++){
-		var altitude = articles[article].id; 
-		var newPosX = (xoff/window.innerWidth)*100;
-		var newPosY = (yoff/window.innerHeight)*100;
-		newPosX = newPosX.toString().concat("%");
-		newPosY = newPosY.toString().concat("%");
-		articles[article].style.left = newPosX;
-		articles[article].style.top = newPosY;
-	}*/
 	for(var article = 0; article < articles.length; article++){
 		articles[article].style.fontSize = (25/scale).toString() + "px";
-		//articles[article].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 	}
 }
 
 function write(newText, x, y){
 	mapCtx.fillText(newText, x, y);
-	//console.log("Wrote " + "'" + newText + "' at " + x + ", " + y);
 }
 
 //call transformation
@@ -140,7 +125,7 @@ map.onmousemove = function(e){
 
 map.onwheel = function(e){
 	e.preventDefault();
-	// take the scale into account with the offset
+	//take the scale into account with the offset
 	if((scale <= 5)&&(scale >= 0.5)){
 		var xs = (e.clientX - xoff) / scale,
 		ys = (e.clientY - yoff) / scale,
